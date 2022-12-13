2f5d9fd081b5cca7aeb89f3dc6da37cdde3ad4c0
Josefreitas788 <josefreitas788@gmail.com>
2022-12-13 15:33:33 -0300

    Código do banco de dados

--- codigo_sql/locadora_de_filmes.sql
+++ codigo_sql/locadora_de_filmes.sql
@@ -18,7 +18,7 @@ create table locacao(
 	id_locatario int not null,
 	data_inicio_locacao date,
 	data_fim_locacao date,
-	entregue  char not null check( entregue = 'S' or entregue = 'N'),
+	ind_entregue  char not null check( entregue = 'S' or entregue = 'N'),
 	
 	constraint fk_filme
 		foreign key(id_filme)
@@ -32,4 +32,72 @@ create table locacao(
 		check(not exists(select id_filme from locacao where id = 'N'))
 	
 )
+create table locacao(
+	id Serial primary key,
+	id_filme int not null,
+	id_locatario int not null,
+	data_inicio_locacao date,
+	data_fim_locacao date,
+	ind_entregue  char not null check( ind_entregue = 'S' or ind_entregue = 'N'),
+	
+	constraint fk_filme
+		foreign key(id_filme)
+			references filme(id),
+			
+	constraint fk_locatario
+		foreign key(id_locatario)
+			references locatario(id),
+	
+--	constraint fk_checa_alugado
+--		check(not exists(select id_filme from locacao where id = 'N'))
+	
+	UNIQUE(id_filme)
+	
+)
+
+create or replace function func_locacao() 
+returns trigger 
+language plpgsql
+as
+$$
+begin
+	if new.ind_entregue = 'S' then 
+		insert into hist_locacao(id,id_filme,id_locacao,data_inicio_locacao,data_fim_locacao,ind_entregue)
+			values(old.id,old.id_filme,old.id_locacao,old.data_inicio_locacao,old.data_fim_locacao,old.ind_entregue);
+		
+		delete from locacao where ind_entregue = 'S';
+	end if;					
+	return new;
+ end;
+$$
+ 
+
+create trigger trigger_locacao_aiu after insert or update 
+on locacao
+for each row
+	execute procedure func_locacao();
+	
+	
+	
+	
+create table hist_locacao( 
+	id int primary key,
+	id_filme int not null,
+	id_locatario int not null,
+	data_inicio_locacao date,
+	data_fim_locacao date,
+	ind_entregue  char not null check( ind_entregue = 'S' or ind_entregue = 'N'),
+	
+	constraint fk_filme
+		foreign key(id_filme)
+			references filme(id),
+			
+	constraint fk_locatario
+		foreign key(id_locatario)
+			references locatario(id)
+
+)
+
+
+
 
