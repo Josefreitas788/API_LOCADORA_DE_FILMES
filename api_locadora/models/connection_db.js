3daa967d4c0a6d17eaa7d1499a49eddcb60b4eb0
Josefreitas788 <josefreitas788@gmail.com>
2022-12-13 17:35:18 -0300

    conexão com db

--- api_locadora/models/connection_db.js
+++ api_locadora/models/connection_db.js
@@ -1,6 +1,5 @@
 const {Sequelize} = require('sequelize');
 
-// var sequelize = new Sequelize('postgres://namlhwumxschsb:61ef3085f22ae14ce13439bb7cdc911d62e102c1fc2b3800688a35bada812be6@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d5t5huhnt06dca');
 const host = 'tuffi.db.elephantsql.com'
 const database = 'bvdrmbjo'
 const user = 'bvdrmbjo'
